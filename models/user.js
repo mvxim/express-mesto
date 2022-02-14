a2459c3b608ad5497d98aa7a779fd88e5d890a04
Maxim Donika <donika.maxim@icloud.com>
2022-02-14 15:32:12 +0300

    feat: create user & card models

--- models/user.js
+++ models/user.js
@@ -7,18 +7,16 @@ const userSchema = new mongoose.Schema({
     minlength: 2,
     maxlength: 30,
   },
-  gender: {
-    type: String, // гендер — это строка
-    enum: ['м', 'ж', 'другой'] // gender может принимать одно из трёх значений
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
   },
-  age: { // у пользователя есть возраст
-    type: Number, // возраст - число
-    validate: { // опишем свойство validate
-      validator(v) { // validator - функция проверки данных. v - значение свойства age
-        return v >= 18; // если возраст меньше 18, вернётся false
-      },
-      message: 'Вам должно быть больше 18 лет!', // когда validator вернёт false, будет использовано это сообщение
-    }
-  },
-  about: String, // тип — String
+  avatar: {
+    type: String,
+    required: true,
+  }
 });
+
+module.exports = mongoose.model('user', userSchema)
